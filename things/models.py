fdb00f0b0301f531f15ce76a8d5bf85ac7566528
Svilen Dilchev <svilen.dilchev@kcl.ac.uk>
2023-11-03 18:55:49 +0200

    Complete Assignment

--- things/models.py
+++ things/models.py
@@ -6,6 +6,6 @@ from django.db.models import Model
 
 
 class Thing(Model):
-    name = models.TextField(default="", unique=True, blank=False, max_length=30, validators=[MaxValueValidator(30)])
-    description = models.TextField(default="", unique=False, blank=True, max_length=120, validators=[MaxValueValidator(120)])
-    quantity = models.IntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
+    name = models.CharField(default="", unique=True, blank=False, max_length=30)
+    description = models.CharField(default="", unique=False, blank=True, max_length=120)
+    quantity = models.IntegerField(default=0 , validators=[MaxValueValidator(100), MinValueValidator(0)])

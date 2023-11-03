3c76dcea49c6711ae77008d74a1b7dc438fef5f5
Svilen Dilchev <svilen.dilchev@kcl.ac.uk>
2023-11-03 18:32:56 +0200

    Fix Bugs

--- things/models.py
+++ things/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import Model
 
@@ -6,6 +6,6 @@ from django.db.models import Model
 
 
 class Thing(Model):
-    name = models.TextField(default="", unique=True, blank=False, max_length=30)
-    description = models.TextField(default="", unique=False, max_length=120)
-    quantity = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
+    name = models.TextField(default="", unique=True, blank=False, max_length=30, validators=[MaxValueValidator(30)])
+    description = models.TextField(default="", unique=False, blank=True, max_length=120, validators=[MaxValueValidator(120)])
+    quantity = models.IntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
